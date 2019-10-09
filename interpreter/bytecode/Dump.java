b6257b216d3dad8d7a99d6daf686e8e79540d99c
Luciana Seljak <lseljak92@gmail.com>
2019-10-08 19:33:12 -0700

    Dump bytecode

--- interpreter/bytecode/Dump.java
+++ interpreter/bytecode/Dump.java
@@ -5,13 +5,24 @@ import interpreter.VirtualMachine;
 import java.util.ArrayList;
 
 public class Dump extends ByteCode {
+    private boolean flag;
+    private String state;
+
     @Override
     public void execute(VirtualMachine vm) {
-
+        if(flag)
+            vm.getRunStack().dump();
     }
 
     @Override
     public void init(ArrayList<String> args) {
+        state = args.get(0);
+        if(state == "ON")
+            flag = true;
+        flag = false;
+    }
 
+    public boolean isFlag() {
+        return flag;
     }
 }
