d3b1be0539fd64515c01dd74f9a3118a9c90dcea
Luciana Seljak <lseljak92@gmail.com>
2019-10-08 19:15:52 -0700

    Add functionality to runtime stack

--- interpreter/RunTimeStack.java
+++ interpreter/RunTimeStack.java
@@ -16,20 +16,37 @@ public class RunTimeStack {
         framePointer.add(0);
     }
 
+    /**
+     * Used for dumping the current state of runTimeStack.
+     * It prints portions of the stack based on respective frame markers.
+     */
     public void dump(){
 
     }
 
+    /**
+     * returns top of runtime stack without removing
+     * @return copy of the top of the stack
+     */
     public int peek(){
         int lastElement = runTimeStack.size()-1;
         return runTimeStack.get(lastElement);
     }
 
+    /**
+     * push value i to top of the stack
+     * @param i value to be pushed
+     * @return value pushed
+     */
     public int push(int i){
         runTimeStack.add(i);
         return i;
     }
 
+    /**
+     * removes top of runtime stack
+     * @return value popped
+     */
     public int pop(){
         int lastElement = runTimeStack.size()-1;
         int valuePopped = runTimeStack.get(lastElement);
@@ -38,25 +55,54 @@ public class RunTimeStack {
         return valuePopped;
     }
 
+    /**
+     * Takes top item of runtime stack and stores it into an offset
+     * starting from current frame
+     * @param offset
+     * @return item just stored
+     */
     public int store(int offset){
         int lastElement = runTimeStack.size()-1;
-
-        return lastElement;
+        int topValue = runTimeStack.get(lastElement);
+        runTimeStack.remove(lastElement);
+        //Store offset starting from current frame
+        runTimeStack.set(framePointer.peek()+offset, topValue);
+        return topValue;
     }
 
+    /**
+     * Takes value from runtime stack that is at offset from current frame
+     * and pushes it onto top of stack
+     * @param offset
+     * @return item just loaded
+     */
     public int load(int offset){
         int value = runTimeStack.get(framePointer.peek()-offset);
         runTimeStack.add(value);
         return value;
     }
 
+    /**
+     * Create a new frame pointer at the index offset slots down from the top
+     * of the runtime stack
+     * @param offset
+     */
     public void newFrameAt(int offset){
         framePointer.add(runTimeStack.size()-offset);
     }
 
+    /**
+     * Pop current frame off the runtime stack. Also removes the frame
+     * pointer value from FramePointer stack
+     */
     public void popFrame(){
+        int lastElement = runTimeStack.size()-1;
+        int value = runTimeStack.get(lastElement);
+        while(runTimeStack.size() != framePointer.peek())
+            runTimeStack.remove(lastElement);
         if(framePointer.peek() != 0)
             framePointer.pop();
+        runTimeStack.add(value);
     }
     
 }
